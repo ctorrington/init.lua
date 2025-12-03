0154e7a22386c898a38df0d2167742442c0337a2
Christopher Torrington <60221027+ctorrington@users.noreply.github.com>
2025-12-03 19:26:50 +0000

    Remove unnecessary Vim options from init.lua

--- init.lua
+++ init.lua
@@ -1,23 +1,2 @@
-vim.opt.termguicolors = true
-
-vim.g.mapleader = " "
-
 require("config.lazy")
 require("config.keymaps")
-
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.opt.smartindent = true
-
-vim.opt.wrap = false
-
-vim.hlsearch = false
-vim.opt.incsearch = true
-
-vim.opt.scrolloff = 8
