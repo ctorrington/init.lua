5b87968f9012b0997cd5801294f60abdfb265c34
Christopher Torrington <60221027+ctorrington@users.noreply.github.com>
2025-12-03 11:07:19 +0000

    Update telecope.lua

--- lua/plugins/telecope.lua
+++ lua/plugins/telecope.lua
@@ -3,9 +3,9 @@ return {
   tag = 'v0.2.0',
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = 'Telescope find files' })
-    vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = 'Telescope live grep' })
-    vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = 'Telescope buffers' })
-    vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = 'Telescope help tags' })
-  }
+    vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = 'Telescope find files' }),
+    vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = 'Telescope live grep' }),
+    vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = 'Telescope buffers' }),
+    vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = 'Telescope help tags' }),
+  },
 }
