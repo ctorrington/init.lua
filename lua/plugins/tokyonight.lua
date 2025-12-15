fbf08c8c64b69bd063af09b8bb0b9ac8f9dcca39
Christopher Torrington <60221027+ctorrington@users.noreply.github.com>
2025-12-15 18:30:16 +0000

    Update tokyonight plugin configuration

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,9 +1,3 @@
-return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    vim.cmd([[colorscheme tokyonight]])
-  end,
-}
+vim.pack.add({"https://github.com/folke/tokyonight.nvim.git"})
+
+vim.cmd("colorscheme tokyonight-storm")
