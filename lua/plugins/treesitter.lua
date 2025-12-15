ed38f23543f95ed7623bcdf3a18ccb955b5139e2
Christopher Torrington <60221027+ctorrington@users.noreply.github.com>
2025-12-15 18:35:10 +0000

    Refactor Treesitter plugin configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,22 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = 'main',
-  lazy = false,
-  build = ":TSUpdate",
-  opts = {
-    ensure_installed = { "python", "lua", "javascript", "html", "markdown", "bash" },
-    highlight = { enable = true }
-  }
-}
+vim.pack.add({
+    {
+      src = "https://github.com/nvim-treesitter/nvim-treesitter.git",
+      version = "master"
+    }
+})
+
+require("nvim-treesitter.configs").setup({
+  ensure_installed = {
+      "lua",
+      "python",
+      "html",
+      "javascript"
+    },
+    auto_install = false,
+    highlight = {
+      enable = true
+    },
+    indent = {
+      enable = true
+    }
+})
