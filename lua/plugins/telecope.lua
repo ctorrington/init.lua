6c29251dabe969673e95d4592c2a6903791357cd
Christopher Torrington <60221027+ctorrington@users.noreply.github.com>
2025-12-15 18:29:25 +0000

    Update telecope.lua

--- lua/plugins/telecope.lua
+++ lua/plugins/telecope.lua
@@ -1,11 +1,30 @@
-return {
-  'nvim-telescope/telescope.nvim',
-  tag = 'v0.2.0',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  keys = {
-    vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files() end, { desc = 'Telescope find files' }),
-    vim.keymap.set('n', '<leader>fg', function() require('telescope.builtin').live_grep() end, { desc = 'Telescope live grep' }),
-    vim.keymap.set('n', '<leader>fb', function() require('telescope.builtin').buffers() end, { desc = 'Telescope buffers' }),
-    vim.keymap.set('n', '<leader>fh', function() require('telescope.builtin').help_tags() end, { desc = 'Telescope help tags' }),
-  },
-}
+vim.pack.add({
+    {
+      src = "https://github.com/nvim-telescope/telescope.nvim.git",
+      version = "master"
+    },
+    {
+      keys = {
+        vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files() end, { desc = 'Telescope find files' }),
+        vim.keymap.set('n', '<leader>fg', function() require('telescope.builtin').live_grep() end, { desc = 'Telescope live grep' }),
+        vim.keymap.set('n', '<leader>fb', function() require('telescope.builtin').buffers() end, { desc = 'Telescope buffers' }),
+        vim.keymap.set('n', '<leader>fh', function() require('telescope.builtin').help_tags() end, { desc = 'Telescope help tags' }),
+      }
+    }
+})
+
+require("nvim-treesitter.configs").setup({
+  ensure_installed = {
+      "lua",
+      "python",
+      "html",
+      "javascript"
+    },
+    auto_install = false,
+    highlight = {
+      enable = true
+    },
+    indent = {
+      enable = true
+    }
+})
