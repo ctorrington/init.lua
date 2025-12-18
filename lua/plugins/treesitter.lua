1964b99c0785b99eb4f8f5d6d445e0091f970d48
Christopher Torrington <60221027+ctorrington@users.noreply.github.com>
2025-12-18 16:05:23 +0000

    Remove Treesitter setup configuration
    Removed Treesitter configuration setup from the plugin file.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,19 +4,3 @@ vim.pack.add({
       version = "master"
     }
 })
-
-require("nvim-treesitter.configs").setup({
-  ensure_installed = {
-      "lua",
-      "python",
-      "html",
-      "javascript"
-    },
-    auto_install = false,
-    highlight = {
-      enable = true
-    },
-    indent = {
-      enable = true
-    }
-})
