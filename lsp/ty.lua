5c7cb0ed899ce122315efd0780bf7cea8b238a9e
Christopher Torrington <60221027+ctorrington@users.noreply.github.com>
2025-12-20 15:42:36 +0000

    Configure LSP completion and autocommands
    Added configuration for LSP completion and handling.

--- lsp/ty.lua
+++ lsp/ty.lua
@@ -1,3 +1,43 @@
+-- Completion window config.
+vim.cmd[[set completeopt+=menuone,popup,noinsert,preview]]
+
+-- Enable LSP when attached.
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local client = assert(
+        vim.lsp.get_client_by_id(args.data.client_id)
+      )
+      vim.lsp.completion.enable(
+        true,
+        client.id,
+        args.buff,
+        {
+          autotrigger = false
+        }
+      )
+    end,
+})
+
+
+-- Handle LSP completion when any character is enterd in insert mode.
+vim.api.nvim_create_autocmd("InsertCharPre", {
+    callback = function(args)
+      -- Exit early if no LSP in current buffer.
+      if next(vim.lsp.get_clients({ bufnr = 0 })) == nill then
+        return
+      end
+
+      -- Exit early if white space is entered.
+      if vim.v.char:match("%s") then
+        return
+      end
+
+      -- Trigger LSP completion.
+      vim.lsp.completion.get()
+    end,
+})
+
+-- Start language server.
 return {
   cmd = { "ty", "server" },
   root_markers = { "pyproject.toml", ".git" },
